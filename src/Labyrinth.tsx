f322b9279cd82f5b3dc840a4bd4d9f5205e986b1
Shawn Taylor <shawn@mapsandapps.net>
2026-05-09 13:47:36 -0400

    Remove demo app

---
+++ src/Labyrinth.tsx
@@ -0,0 +1,10 @@
+// import { useState } from "react";
+import "./Labyrinth.css";
+
+function Labyrinth() {
+  // const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default Labyrinth;
