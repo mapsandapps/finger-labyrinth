f7de8c0879572002b53dd30635ae5d9822a0fee7
Shawn Taylor <shawn@mapsandapps.net>
2026-05-09 18:32:26 -0400

    Animate out after going in

--- src/Labyrinth.tsx
+++ src/Labyrinth.tsx
@@ -1,19 +1,30 @@
 import { useCallback, useRef, useState } from "react";
 import "./Labyrinth.css";
 
+type Direction = "in" | "out";
+
 const PATH =
   "M 512,64 a 192,192 0 0,1 192,192 a 192,192 0 0,1 -192,192 a 256,256 0 0,0 256,-256 a 320,320 0 0,1 -320,320 a 192,192 0 0,1 -192,-192 a 128,128 0 0,0 128,128 a 64,64 0 0,0 64,-64 a 64,64 0 0,1 64,-64 a 64,64 0 0,0 64,-64 a 64,64 0 0,0 -64,-64 a 64,64 0 0,0 -64,64 a 64,64 0 0,1 -64,64 a 128,128 0 0,1 -128,-128";
-
-const ANIMATION_OPTIONS = {
-  duration: 10000,
-  easing: "linear",
-  fill: "both" as FillMode,
-};
+const SPEED = 3.5;
 
 function Labyrinth() {
+  const [direction, setDirection] = useState<Direction>("in");
   const [pathAnimation, setPathAnimation] = useState<Animation>();
   const [circleAnimation, setCircleAnimation] = useState<Animation>();
   const currentLocationRef = useRef<SVGCircleElement>(null);
+
+  const getAnimationOptions = (): KeyframeAnimationOptions => {
+    const pathLength = (
+      document.querySelector("path") as SVGPathElement
+    ).getTotalLength();
+    const duration = pathLength ? pathLength * SPEED : 10000;
+
+    return {
+      duration,
+      easing: "linear",
+      fill: "both",
+    };
+  };
 
   const setIconVisible = () => {
     if (currentLocationRef.current) {
@@ -52,44 +63,59 @@ function Labyrinth() {
     circleAnimation.pause();
   };
 
-  const initPath = useCallback((el: SVGPathElement) => {
-    if (el !== null) {
-      // get the length of the path and set up the dash so it starts 'empty'
-      const lineLength = el.getTotalLength();
-      el.style.strokeDasharray = lineLength + " " + lineLength;
-      el.style.strokeDashoffset = "0";
+  const initPath = useCallback(
+    (el: SVGPathElement) => {
+      if (el !== null) {
+        // get the length of the path and set up the dash so it starts 'empty'
+        const lineLength = el.getTotalLength();
+        el.style.strokeDasharray = lineLength + " " + lineLength;
+        el.style.strokeDashoffset = "0";
 
-      const pathAnim = el.animate(
-        [{ strokeDashoffset: `${lineLength}` }, { strokeDashoffset: "0" }],
-        ANIMATION_OPTIONS,
-      );
-      pathAnim.currentTime = 0; // prevent a flash of the finished animation
-      pathAnim.pause(); // will start playing on load if not paused
+        const keyframes =
+          direction === "in"
+            ? [{ strokeDashoffset: `${lineLength}` }, { strokeDashoffset: "0" }]
+            : [
+                { strokeDashoffset: "0" },
+                { strokeDashoffset: `${lineLength}` },
+              ];
 
-      // when animation ends, prep everything to be restarted
-      pathAnim.onfinish = () => {
-        setIconVisible();
-      };
+        const pathAnim = el.animate(keyframes, getAnimationOptions());
+        pathAnim.currentTime = 0; // prevent a flash of the finished animation
+        pathAnim.pause(); // will start playing on load if not paused
 
-      setPathAnimation(pathAnim);
-    }
-  }, []);
+        // when animation ends, prep everything to be restarted
+        pathAnim.onfinish = () => {
+          console.log("onfinish: TODO: reverse animation to leave labyrinth");
+          setDirection(direction === "in" ? "out" : "in");
+          setIconVisible();
+        };
 
-  const initCircle = useCallback((el: SVGCircleElement) => {
-    if (el !== null) {
-      el.style.offsetPath = `path("${PATH}")`;
-      // NOTE: could set offsetRotate here to `0deg` but not needed for a circle
+        setPathAnimation(pathAnim);
+      }
+    },
+    [direction],
+  );
 
-      const circleAnim = el.animate(
-        [{ offsetDistance: "0%" }, { offsetDistance: "100%" }],
-        ANIMATION_OPTIONS,
-      );
-      circleAnim.currentTime = 0; // prevent a flash of the finished animation
-      circleAnim.pause(); // will start playing on load if not paused
+  const initCircle = useCallback(
+    (el: SVGCircleElement) => {
+      if (el !== null) {
+        el.style.offsetPath = `path("${PATH}")`;
+        // NOTE: could set offsetRotate here to `0deg` but not needed for a circle
 
-      setCircleAnimation(circleAnim);
-    }
-  }, []);
+        const keyframes =
+          direction === "in"
+            ? [{ offsetDistance: "0%" }, { offsetDistance: "100%" }]
+            : [{ offsetDistance: "100%" }, { offsetDistance: "0%" }];
+
+        const circleAnim = el.animate(keyframes, getAnimationOptions());
+        circleAnim.currentTime = 0; // prevent a flash of the finished animation
+        circleAnim.pause(); // will start playing on load if not paused
+
+        setCircleAnimation(circleAnim);
+      }
+    },
+    [direction],
+  );
 
   return (
     <>
